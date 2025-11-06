034a4b68b55c14e5b14cc2381af18f674fe25cde
Shahid Afrid <shahidafridsa08@gmail.com>
2025-11-06 06:28:30 +0000

    The error means PM2 is starting Node with a preload of module-alias/regi

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,8 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
+        ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
