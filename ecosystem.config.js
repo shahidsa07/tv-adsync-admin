7c6ea1fcbaff146b54c2edb218a90713e86d25cd
Shahid Afrid <shahidafridsa08@gmail.com>
2025-11-06 07:02:35 +0000

    Your package.json should look like this:
    "scripts": {
      "dev": "next de

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'signagewise-app',
-      script: './dist/server.js',
+      script: 'next',
+      args: 'start -p 8080',
       // Set the current working directory to the project root
       cwd: '.',
       // Ensure the app uses the environment variables from the user's shell
